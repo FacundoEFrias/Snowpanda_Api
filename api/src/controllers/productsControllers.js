b28662cdbffbe7899443c433b59f08bd883ee7eb
Facundo Frias <97521604+FacundoEFrias@users.noreply.github.com>
2023-09-14 11:40:42 +0200

    Update productsControllers.js

--- api/src/controllers/productsControllers.js
+++ api/src/controllers/productsControllers.js
@@ -34,12 +34,12 @@ const getProductsByBrand = async (req, res) => {
   try {
     const allData = await Products.findAll({});
     const productsBrand = allData.filter((e) => e.brand.brandName === access);
-    if(productsBrands.length === 0) {
-      res.status(404).json({message: "Productos no encontrados")};
-    }else {
-      
+    if(productsBrand.length === 0) {
+      res.status(404).json({ message: "Productos no encontrados" )};
+    } else {
     res.status(200).json(productsBrand);
-    } } catch (err) {
+    }
+  } catch (err) {
     res.status(500).json(err.message);
   }
 };
